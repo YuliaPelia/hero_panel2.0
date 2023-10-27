cd495c95c232a4e738aaef27a37463f07e87aaba
Yulchikss <yuliapelia19@gmail.com>
2023-10-27 13:53:47 +0300

    create store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,42 @@
-import { createStore, combineReducers } from 'redux';
-import heroes from '../reducers/heroes';
-import filters from '../reducers/filters';
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+import heroes from "../reducers/heroes";
+import filters from "../reducers/filters";
+import { miniSerializeError } from "@reduxjs/toolkit";
 
-const store = createStore( combineReducers({heroes, filters}),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const stringMiddleware = () => (next) => (action) => {
+  if (typeof action === "string") {
+    return next({
+      type: action,
+    });
+  }
+  return next(action);
+};
+
+const enhancer =
+  (createStore) =>
+  (...args) => {
+    const store = createStore(...args);
+
+    const oldDispatch = store.dispatch;
+    store.dispatch = (action) => {
+      if (typeof action === "string") {
+        return oldDispatch({
+          type: action,
+        });
+      }
+      return oldDispatch(action);
+    };
+    return store;
+  };
+
+const store = createStore(
+  combineReducers({ heroes, filters }),
+  compose(
+    applyMiddleware(stringMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+// const store = createStore(combineReducers({ heroes, filters }), enhancer);
 
 export default store;
+//
