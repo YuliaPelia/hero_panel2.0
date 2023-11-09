269da565f45ab1df1491ae48b37c88df6f835d62
Yulchikss <yuliapelia19@gmail.com>
2023-11-09 13:33:19 +0200

    used createAsyncThunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,21 +1,9 @@
 import {
-  heroesFetching,
-  heroesFetched,
-  heroesFetchingError,
-} from "../components/heroesList/heroesSlice";
-
-import {
   filtersFetched,
   filtersFetching,
   filtersFetchingError,
 } from "../components/heroesFilters/filterSlice";
 
-export const fetchHeroes = (request) => (dispatch) => {
-  dispatch(heroesFetching());
-  request("http://localhost:3001/heroes")
-    .then((data) => dispatch(heroesFetched(data)))
-    .catch(() => dispatch(heroesFetchingError()));
-};
 export const fetchFilters = (request) => (dispatch) => {
   dispatch(filtersFetching());
   request("http://localhost:3001/filters")
