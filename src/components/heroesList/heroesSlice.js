8bf5119172fbb56cf118aefaec48ebe4f22ed194
Yulchikss <yuliapelia19@gmail.com>
2023-11-11 18:44:18 +0200

    add adapter to project

--- src/components/heroesList/heroesSlice.js
+++ src/components/heroesList/heroesSlice.js
@@ -1,10 +1,23 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import {
+  createSlice,
+  createAsyncThunk,
+  createEntityAdapter,
+  createSelector,
+} from "@reduxjs/toolkit";
 import { useHttp } from "../../hooks/http.hook";
 
-const initialState = {
-  heroes: [],
+const heroesAdapter = createEntityAdapter();
+
+// const initialState = {
+//   heroes: [],
+//   heroesLoadingStatus: "idle",
+// };
+
+const initialState = heroesAdapter.getInitialState({
   heroesLoadingStatus: "idle",
-};
+});
+
+console.log(initialState);
 
 export const fetchHeroes = createAsyncThunk("heroes/fetchHeroes", async () => {
   const { request } = useHttp();
@@ -16,10 +29,10 @@ const heroesSlice = createSlice({
   initialState,
   reducers: {
     heroCreated: (state, action) => {
-      state.heroes.push(action.payload);
+      heroesAdapter.addOne(state, action.payload);
     },
     heroDeleted: (state, action) => {
-      state.heroes = state.heroes.filter((item) => item.id !== action.payload);
+      heroesAdapter.removeOne(state, action.payload);
     },
   },
   extraReducers: (builder) => {
@@ -29,7 +42,7 @@ const heroesSlice = createSlice({
       })
       .addCase(fetchHeroes.fulfilled, (state, action) => {
         state.heroesLoadingStatus = "idle";
-        state.heroes = action.payload;
+        heroesAdapter.setAll(state, action.payload);
       })
       .addCase(fetchHeroes.rejected, (state) => {
         state.heroesLoadingStatus = "error";
@@ -41,6 +54,22 @@ const heroesSlice = createSlice({
 const { actions, reducer } = heroesSlice;
 
 export default reducer;
+
+const { selectAll } = heroesAdapter.getSelectors((state) => state.heroes);
+
+export const filteredHeroesSelector = createSelector(
+  (state) => state.filters.activeFilter,
+  selectAll,
+  (filter, heroes) => {
+    if (filter === "all") {
+      console.log("render");
+      return heroes;
+    } else {
+      return heroes.filter((item) => item.element === filter);
+    }
+  }
+);
+
 export const {
   heroesFetching,
   heroesFetched,
